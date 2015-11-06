6fb490f8d64a2735d55aa6e521d1d11359c0fe22
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-11-06 15:07:02 +0100

    add some byte array helpers

---
+++ src/main/java/util/ArrayUtils.java
@@ -0,0 +1,62 @@
+package util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayUtils {
+    public static byte[] join(byte delimiter, byte[]... byteArrays) {
+        int totalLength = 0;
+        for (byte[] byteArray : byteArrays) {
+            if (totalLength > 0) { // delimiter
+                ++totalLength;
+            }
+            totalLength += byteArray.length;
+        }
+
+        int offset = 0;
+        final byte[] outputArray = new byte[totalLength];
+        for (byte[] inputArray : byteArrays) {
+            if (offset > 0) { // delimiter
+                outputArray[offset] = delimiter;
+                ++offset;
+            }
+
+            System.arraycopy(inputArray, 0, outputArray, offset, inputArray.length);
+            offset += inputArray.length;
+        }
+
+        return outputArray;
+    }
+
+    public static List<byte[]> split(byte[] bytes, byte delimiter) {
+        final List<byte[]> byteArrays = new LinkedList<>();
+
+        int offset = 0;
+        while (offset != bytes.length) {
+            int nextDelimiterIndex = indexOf(bytes, delimiter, offset);
+            if (nextDelimiterIndex < 0) {
+                nextDelimiterIndex = bytes.length;
+            }
+
+            final int length = nextDelimiterIndex - offset;
+            assert length >= 0;
+
+            final byte[] byteArray = new byte[length];
+            System.arraycopy(bytes, offset, byteArray, 0, length);
+            byteArrays.add(byteArray);
+
+            offset = nextDelimiterIndex;
+        }
+
+        return byteArrays;
+    }
+
+    public static int indexOf(byte[] bytes, byte b, int offset) {
+        for (int i = offset; i < bytes.length; ++i) {
+            if (bytes[i] == b) {
+                return i;
+            }
+        }
+        return -1; // not found
+    }
+}

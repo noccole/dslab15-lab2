9ee6b81b000e5cbe393133b51395f13dd8b24d89
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-12-07 09:28:18 +0100

    add marshalling test for send message request/response

--- src/test/java/marshalling/MarshallingTest.java
+++ src/test/java/marshalling/MarshallingTest.java
@@ -2,6 +2,8 @@ package marshalling;
 
 import messages.LoginRequest;
 import messages.LoginResponse;
+import messages.SendMessageRequest;
+import messages.SendMessageResponse;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -43,6 +45,38 @@ public abstract class MarshallingTest {
 
         // WHEN
         final LoginResponse unmarshalledResponse = LoginResponse.class.cast(marshaller.unmarshall(data));
+
+        // THEN
+        assertEquals(response, unmarshalledResponse);
+    }
+
+    @Test
+    public void testSendMessageRequest() throws MarshallingException {
+        // PREPARE
+        final SendMessageRequest request = new SendMessageRequest();
+        request.setMessage("this is a test message");
+
+        final byte[] data = marshaller.marshall(request);
+
+        // WHEN
+        final SendMessageRequest unmarshalledRequest = SendMessageRequest.class.cast(marshaller.unmarshall(data));
+
+        // THEN
+        assertEquals(request, unmarshalledRequest);
+    }
+
+    @Test
+    public void testSendMessageResponse() throws MarshallingException {
+        // PREPARE
+        final SendMessageRequest request = new SendMessageRequest();
+        request.setMessage("this is a test message");
+
+        final SendMessageResponse response = new SendMessageResponse(request);
+
+        final byte[] data = marshaller.marshall(response);
+
+        // WHEN
+        final SendMessageResponse unmarshalledResponse = SendMessageResponse.class.cast(marshaller.unmarshall(data));
 
         // THEN
         assertEquals(response, unmarshalledResponse);
